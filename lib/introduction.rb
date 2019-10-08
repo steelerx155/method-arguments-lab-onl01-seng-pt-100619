3f3d0cfbddc1186e1599f02c31c2edaebe5f312f
Randy Steele <steelerx155@gmail.com>
2019-10-08 14:55:59 +0000

    Automatically backed up by Learn

--- lib/introduction.rb
+++ lib/introduction.rb
@@ -3,6 +3,6 @@ puts "Hi, my name is #{name}."
 end
 
 def introduction_with_language(name, language)
-  puts "Him mu name is #{name} and I am learning to program in Ember js./n"
+  puts "Him my name is #{name} and I am learning to program in #{language}./n"
   introduction_with_language("Dan", "Ember js")
 end
